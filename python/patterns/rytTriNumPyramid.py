abf60946db967e0fb762c6e24c5269a4c10e2222
Nandishpb <nandish.22ai026@sode-edu.in>
2026-04-25 13:55:26 +0530

    Right angle number pyramid

---
+++ python/patterns/rytTriNumPyramid.py
@@ -0,0 +1,10 @@
+class RightTriangleNumPyramid:
+    def numPattern(self, n):
+        for i in range(n+1):
+            for j in range(1, i+1):
+                print(j, end = " ")
+            print()
+
+sol = RightTriangleNumPyramid()
+n = 6
+sol.numPattern(n)
