9c7e589d4f70e02438baae2c2881970f1c12c1ee
Nandishpb <nandish.22ai026@sode-edu.in>
2026-04-26 23:31:29 +0530

    Updated Square pattern with own logic

--- python/patterns/square.py
+++ python/patterns/square.py
@@ -1,9 +1,8 @@
 class Square:
     def pattern(self, n):
         for i in range(n):
-            for j in range(n):
-                print("*", end =' ')
-            print()
+            print("* " * n)
+        print()
 
          
 
