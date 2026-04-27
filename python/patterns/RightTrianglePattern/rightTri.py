de2973d3dc142c4835bac9b6859365b88a894304
Nandishpb <nandish.22ai026@sode-edu.in>
2026-04-27 18:44:28 +0530

    Code Updated with Single For loop and Double for loop

--- python/patterns/RightTrianglePattern/rightTri.py
+++ python/patterns/RightTrianglePattern/rightTri.py
@@ -1,7 +1,14 @@
+# class RightTriangle:
+#     def pattern(self, n):
+#         for i in range(n):
+#             print(" * " * (i+1))
+
 class RightTriangle:
     def pattern(self, n):
         for i in range(n):
-            print(" * " * (i+1))
+            for j in range(i + 1):
+                print("*", end = " ")
+            print()
 
 sol = RightTriangle()
 n = 5
