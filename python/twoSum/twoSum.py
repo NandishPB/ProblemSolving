d76d95692109a8fb61d55c80bd2680638bcda981
Nandishpb <nandish.22ai026@sode-edu.in>
2026-05-01 00:00:15 +0530

    Updated Two sum problem code with brute force and Optimized code

--- python/twoSum/twoSum.py
+++ python/twoSum/twoSum.py
@@ -1,14 +1,19 @@
 class TwoSum:
     def toSum(self, nums, target):
-        hashmap = {}
+        # hashmap = {}
 
-        for i, num in enumerate(nums):
-            diff = target - num
+        # for i, num in enumerate(nums):
+        #     diff = target - num
 
-            if diff in hashmap:
-                return [hashmap[diff], i]
+        #     if diff in hashmap:
+        #         return [hashmap[diff], i]
             
-            hashmap[num] = i
+        #     hashmap[num] = i
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
 
 arrlist = list(map(int, input("Enter the Nums ").split()))
 target = int(input("Enter the target value "))
